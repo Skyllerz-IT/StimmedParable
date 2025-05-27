e8adc883ab2cca3f33d9cac0d7da376ef48dad2e
Amabile <nikoamazon88@gmail.com>
2025-05-27 10:37:10 +0200

    modified crouch

--- Assets/Scripts/Crouch.cs
+++ Assets/Scripts/Crouch.cs
@@ -1,27 +1,35 @@
 using UnityEngine;
 
-
 public class Crouch : MonoBehaviour
 {
-    private Vector3 crouchScale = new Vector3(0.8f, 0.5f, 0.8f);
+    private Vector3 crouchScale = new Vector3(0.8f, 0.35f, 0.8f);
     private Vector3 playerScale = new Vector3(0.8f, 0.8f, 0.8f);
     private bool isCrouching = false;
+
+    private PlayerMove playerMovement;
+    private float normalSpeed;
+    public float crouchSpeedMultiplier = 0.5f; // Dimezza la velocità
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMove>();
+        normalSpeed = playerMovement.moveSpeed;
+    }
 
     public void ToggleCrouch()
     {
         if (!isCrouching)
         {
-            // Abbassa il personaggio
             transform.localScale = crouchScale;
             transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            playerMovement.moveSpeed = normalSpeed * crouchSpeedMultiplier;
             isCrouching = true;
-            print(transform.localScale);
         }
         else
         {
-            // Rialza il personaggio
             transform.localScale = playerScale;
             transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+            playerMovement.moveSpeed = normalSpeed;
             isCrouching = false;
         }
     }
